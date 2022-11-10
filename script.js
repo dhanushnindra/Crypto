95e1f0f09cad2e69b7689ea7a5faea6475f7ed14
dhanushnindra <dhanushnindraedu@gmail.com>
2022-11-10 22:38:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=1&convert=USD&CMC_PRO_API_KEY=59061bf5-18f0-4604-8f07-0fef5bbed287"
+const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=7&convert=USD&CMC_PRO_API_KEY=59061bf5-18f0-4604-8f07-0fef5bbed287"
 fetch(url,{mode:'cors'})
   .then((res) => res.json())
-  .then((json) => console.log(json.data[0].name);
+  .then((json) => {
+    for (let coin in json.data){
+      
+      //console.log(coin.name)
+      let name = coin.name
+      let price = coin.quote.USD.price
+      
+      console.log(name + " : " + price);
+    }
+    
+  });
