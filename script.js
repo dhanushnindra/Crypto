418ef0c80b62e6db8e8daafa18de572afa2fedac
dhanushnindra <dhanushnindraedu@gmail.com>
2022-11-12 00:03:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=7&convert=USD&CMC_PRO_API_KEY=59061bf5-18f0-4604-8f07-0fef5bbed287"
 
-fetch(url,{mode:'no-cors'})
+fetch(url,{mode:'cors'})
   .then((res) => console.log(res));
 
 fetch(url,{mode:'no-cors'})
