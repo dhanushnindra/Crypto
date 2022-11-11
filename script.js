d41a6696cf26051280a45c35ecc2cf981b7eeb31
dhanushnindra <dhanushnindraedu@gmail.com>
2022-11-12 00:37:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=7&convert=USD&CMC_PRO_API_KEY=59061bf5-18f0-4604-8f07-0fef5bbed287"
 
-fetch(url,{mode:'cors',
-          headers:{"Origin": 'https://dhanushnindra.github.io/Crypto',
-          "Access-Control-Allow-Origin": "*"}})
-  .then((res) => console.log(res));
-
+fetch(url, {
+  mode: 'cors',
+  headers: {
+    'Access-Control-Allow-Origin':'*'
+  }
+})
+  .then(response => response.json())
+  .then(data => {console.log(data)});
 fetch(url,{mode:'no-cors',
           Origin: 'https://dhanushnindra.github.io/Crypto'})
   .then((res) => res.json())
