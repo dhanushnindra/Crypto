1f6e38e67b71ae0c74d1b878d452694c71644634
dhanushnindra <dhanushnindraedu@gmail.com>
2022-11-13 18:36:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,21 +10,19 @@ const url = "https://gentle-woodland-70534.herokuapp.com/";
 //           response.json()})
 //   .then(data => {console.log(data)});
 fetch(url)
-  .then(res => {
-          //console.log(res.json());
-          res.json()
-  })
-  .then(json => console.log(json))
-//   .then(obj => {
-//     console.log(obj)
-//     for (let coin of obj.data){
-      
-//       //console.log(coin.name)
-//       let name = coin.name
-//       let price = coin.quote.USD.price
-      
-//       console.log(name + " : " + price);
-//     }
+  .then(response => res.json())
+  .then(response => {
+  
+    console.log(response)
     
-//  })
+    for (let coin of response.data){
+      
+      //console.log(coin.name)
+      let name = coin.name
+      let price = coin.quote.USD.price
+      
+      console.log(name + " : " + price);
+    }
+    
+ })
 .catch((err) => console.log(err));
