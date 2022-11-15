7c017be4ca6070cd9542692ac63329b82e2ea847
dhanushnindra <dhanushnindraedu@gmail.com>
2022-11-15 18:47:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ box.className = " w-full text-sm text-left text-gray-500 shadow-md rounded-md mx
 box.innerHTML = "<thead class='text-xs uppercase bg-gray-700 text-gray-400'> <tr> <th scope='col' class='py-3 px-6'>Coin</th> <th scope='col' class='py-3 px-6'>Price</th> </tr> </thead>";
 
 box.setAttribute('id', 'myBox');
-box.className = " rounded-lg mx-4 my-4  px-4 py-4 bg-green-200 ";
+box.className = " rounded-lg  px-4 py-4 bg-green-200 ";
 
 let boxBody = document.createElement("tbody");
 boxBody.className = "bg-slate-600 border-gray-200 ";
