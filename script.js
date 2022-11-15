050c5696cd8de59a37be63c8ddc71167f4d08798
dhanushnindra <dhanushnindraedu@gmail.com>
2022-11-15 18:15:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ boxBody.className = "bg-slate-600 border-gray-200 ";
 function addCoinCard(CoinName, CoinPrice){
 
     let CoinCard = document.createElement("tr");
-    CoinCard.className = "px-4 py-2 hover:shadow-xl"
+    CoinCard.className = "text-slate-100 px-4 py-2 hover:shadow-xl"
 
     const name = document.createElement("td");
     name.innerHTML += CoinName;
