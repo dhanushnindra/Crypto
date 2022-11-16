9f392588296218bdd7c99b3a6c0b2498eea1c80c
dhanushnindra <dhanushnindraedu@gmail.com>
2022-11-16 14:59:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const url = "https://gentle-woodland-70534.herokuapp.com/";
 let box = document.createElement("table");
 box.className = " w-full text-sm text-left text-emerald-500 shadow-md rounded-md ";
 
-box.innerHTML = "<thead class='text-xs uppercase bg-emerald-700 text-emerald-400'> <tr> <th scope='col' class='py-3 px-6'>Coin</th> <th scope='col' class='py-3 px-6'>Price</th> </tr> </thead>";
+box.innerHTML = "<thead class='text-xs uppercase bg-emerald-700 text-emerald-400'> <tr> <th scope='col' class='py-6 px-6'>Coin</th> <th scope='col' class='py-6 px-6'>Price</th> </tr> </thead>";
 
 box.setAttribute('id', 'myBox');
 //box.className = " rounded-lg  px-4 py-4 bg-green-200 ";
